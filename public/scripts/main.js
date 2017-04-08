3b05618fe261ac8b8e7d1f3e825b5142b004f676
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-04-07 23:51:10 -0400

    trying some new mapping stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,17 @@
 "use strict";
 
-// short -- 110
-// med -- 150
-// long -- 210
-
 var ipsumApp = {};
 
-var ipsumList = ["hoser", "two-four", "double double", "eh", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land"];
+var ipsumList = ["hoser", "two-four", "double double", "eh", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land"];
 
 console.log('hello world');
 
 // create empty array to push new words onto
-var newRandomArray = [];
+var newRandomArray = new Array();
+
+// 1. function: generate random numbers that match button and push to new array
+// e.g. generate 24 random numbers (attach to onClick function)
+// 2. new function: 
 
 // create variables that return varying numbers of words
 // and push these words onto newRandomArray
@@ -19,14 +19,32 @@ var newRandomArray = [];
 var makeShort = function makeShort() {
 	for (var i = 0; i < 24; i = i + 1) {
 		// generate a random number
-		var randomWord = ipsumList[Math.floor(Math.random() * ipsumList.length)];
-		// let randomWord = ipsumList[randomNumber];
-		// let randomWord = ipsumList[randomNumber];
-		// add that word to new array called fullArray
-		var fullArray = newRandomArray + newRandomArray.push(randomWord);
-		console.log('full array', fullArray);
+		var randomNumber = Math.floor(Math.random() * ipsumList.length);
+		var randomWord = ipsumList[randomNumber];
+		// add that word to new array called newRandomArray
+		newRandomArray.push(randomWord);
+		console.log('new random array', newRandomArray);
+		newRandomArray.map(function (i) {
+			console.log('mapping!', i);
+		});
+		var paragraph = newRandomArray.join(' ');
+		console.log("paragraph!", paragraph);
 	}
 };
+
+var randomNumbers = function randomNumbers() {
+	for (var i = 0; i < 24; i = i + 1) {
+		var randomNumber = Math.floor(Math.random() * ipsumList.length);
+		console.log('random numbers', randomNumbers);
+	}
+};
+
+// let punctuationArray = () => {
+// 	for (i = 7; i < 24; i += 8) {
+// 		newRandomArray[i] += ". ";
+// 	} 
+// }
+// console.log('punctuation', punctuationArray());
 
 var makeMedium = function makeMedium() {
 	for (var i = 0; i < 48; i = i + 1) {
@@ -44,22 +62,14 @@ var makeLong = function makeLong() {
 	}
 };
 
-// let punctuation = (fullArrayParam) => {
-// 	for (let i = 7; i <fullArrayParam.length; i += 8) {
-// 		fullArrayParam[i] += '.';
-// 	} console.log('punctuation', punctuation());		
-// }
+// sort into groups of 8 (8 words per sentence)
+// after every 8th item (7, 15, 23) add a period to end sentence
 
-
-// punctuation();
 
 // capitalize first letter in each sentence
 
 // how to add punctuation? Cupcake Ipsum?
 
-
-// Create a div with a class of generatedIpsum inside section with a class of displayIpsum and insert fullArray inside
-// $('section.displayIpsum').html('<div class="generatedIpsum"><p>' + fullArrayParam + '</p></div>');
 
 // click event
 ipsumApp.events = function () {
@@ -67,6 +77,7 @@ ipsumApp.events = function () {
 		e.preventDefault();
 		console.log('clicked short');
 		makeShort();
+		randomNumbers();
 	});
 	$('#button__medium').on('click', function (e) {
 		e.preventDefault();
@@ -88,7 +99,6 @@ ipsumApp.events = function () {
 // then periods are concatenated after every 8? words
 // commas?
 // paragraph is displayed on the page
-
 
 ipsumApp.init = function () {}; //end of init();
 
