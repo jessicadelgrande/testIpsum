d14cd68b92b65479e56acc90bd1bc3ca4b1ea2d0
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-04-08 11:50:12 -0400

    basically working, just need to roll out to medium and large buttons and add punctuation and a reset button and styling.

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,6 +1,6 @@
 const ipsumApp = {}
 
-const ipsumList = [ "hoser", "two-four", "double double", "eh", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land"
+const ipsumList = [ "hoser", "two-four", "double double", "eh", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land", "we the north", "true north strong and free"
 ];
 
 console.log('hello world');
@@ -8,35 +8,29 @@ console.log('hello world');
 // create empty array to push new words onto
 let newRandomArray = new Array();
 
-// 1. function: generate random numbers that match button and push to new array
-// e.g. generate 24 random numbers (attach to onClick function)
-// 2. new function: 
-
 // create variables that return varying numbers of words
 // and push these words onto newRandomArray
 // for use in onClick events
-let makeShort = () => {
+let generateRandom = () => {
 	for (let i = 0; i < 24; i = i + 1) {
 		// generate a random number
 		let randomNumber = Math.floor(Math.random() * ipsumList.length);
 		let randomWord = ipsumList[randomNumber];
-		// add that word to new array called newRandomArray
+		console.log('random word', randomWord);
+		// add that word to new empty array called newRandomArray
 		newRandomArray.push(randomWord);
-		console.log('new random array', newRandomArray);
-		newRandomArray.map(function	(i) {
-			console.log('mapping!', i);
-		});
+		console.log('new array', newRandomArray);
 		let paragraph = newRandomArray.join(' ');
-		console.log("paragraph!", paragraph);
+		$("section.displayIpsum").html(`<div class='completeParagraph'><p> ${paragraph} </p></div>`);
+		// for (let i = 7; i < 24; i += 8) {
+		// 	newRandomArray[i] += '.';
+		// }
 	}
-}
+} 
 
-let randomNumbers = () => {
-	for (let i = 0; i < 24; i = i + 1) {
-		let randomNumber = Math.floor(Math.random() * ipsumList.length);
-		console.log('random numbers', randomNumbers);
-	}
-}
+
+
+
 
 
 
@@ -82,8 +76,7 @@ ipsumApp.events = () => {
 	$('#button__short').on('click', (e) => {
 		e.preventDefault();
 		console.log('clicked short');
-		makeShort();
-		randomNumbers();
+		generateRandom();
 	});
 	$('#button__medium').on('click', (e) => {
 		e.preventDefault();
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,40 +2,30 @@
 
 var ipsumApp = {};
 
-var ipsumList = ["hoser", "two-four", "double double", "eh", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land"];
+var ipsumList = ["hoser", "two-four", "double double", "eh", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land", "we the north", "true north strong and free"];
 
 console.log('hello world');
 
 // create empty array to push new words onto
 var newRandomArray = new Array();
 
-// 1. function: generate random numbers that match button and push to new array
-// e.g. generate 24 random numbers (attach to onClick function)
-// 2. new function: 
-
 // create variables that return varying numbers of words
 // and push these words onto newRandomArray
 // for use in onClick events
-var makeShort = function makeShort() {
+var generateRandom = function generateRandom() {
 	for (var i = 0; i < 24; i = i + 1) {
 		// generate a random number
 		var randomNumber = Math.floor(Math.random() * ipsumList.length);
 		var randomWord = ipsumList[randomNumber];
-		// add that word to new array called newRandomArray
+		console.log('random word', randomWord);
+		// add that word to new empty array called newRandomArray
 		newRandomArray.push(randomWord);
-		console.log('new random array', newRandomArray);
-		newRandomArray.map(function (i) {
-			console.log('mapping!', i);
-		});
+		console.log('new array', newRandomArray);
 		var paragraph = newRandomArray.join(' ');
-		console.log("paragraph!", paragraph);
-	}
-};
-
-var randomNumbers = function randomNumbers() {
-	for (var i = 0; i < 24; i = i + 1) {
-		var randomNumber = Math.floor(Math.random() * ipsumList.length);
-		console.log('random numbers', randomNumbers);
+		$("section.displayIpsum").html("<div class='completeParagraph'><p> " + paragraph + " </p></div>");
+		// for (let i = 7; i < 24; i += 8) {
+		// 	newRandomArray[i] += '.';
+		// }
 	}
 };
 
@@ -76,8 +66,7 @@ ipsumApp.events = function () {
 	$('#button__short').on('click', function (e) {
 		e.preventDefault();
 		console.log('clicked short');
-		makeShort();
-		randomNumbers();
+		generateRandom();
 	});
 	$('#button__medium').on('click', function (e) {
 		e.preventDefault();
