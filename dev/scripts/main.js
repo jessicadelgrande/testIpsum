d9f078fb02878bcf6bec8b139fe82fc9c7e10cd4
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-04-15 00:33:37 -0400

    all functionality is perfect, needs to be styled

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -3,7 +3,7 @@ const ipsumApp = {}
 const ipsumList = [ "hoser", "two-four", "double-double", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land", "we the north", "true north strong and free", "the Group of Seven", "brewski"
 ];
 
-console.log('hello world');
+console.log("hello world");
 
 // create empty array for new words
 let newRandomArray = [];
@@ -38,7 +38,7 @@ const shortParagraph = () => {
 		generateRandomWords();
 	}
 	let finalShort = combinedSentences.join(" ");
-	$("section.displayIpsum").html(`<div class='completeParagraph'><p> ${finalShort} </p></div>`);
+	$("section.displayIpsum").html(`<div class="completeParagraph"><p> ${finalShort} </p></div>`);
 }
 
 const mediumParagraph = () => {
@@ -46,7 +46,7 @@ const mediumParagraph = () => {
 		generateRandomWords();
 	}
 	let finalMedium = combinedSentences.join(" ");
-	$("section.displayIpsum").html(`<div class='completeParagraph'><p> ${finalMedium} </p></div>`);
+	$("section.displayIpsum").html(`<div class="completeParagraph"><p> ${finalMedium} </p></div>`);
 }
 
 const longParagraph = () => {
@@ -54,25 +54,33 @@ const longParagraph = () => {
 		generateRandomWords();
 	}
 	let finalLong = combinedSentences.join(" ");
-	$("section.displayIpsum").html(`<div class='completeParagraph'><p> ${finalLong} </p></div>`);
+	$("section.displayIpsum").html(`<div class="completeParagraph"><p> ${finalLong} </p></div>`);
 }
 
 // click events
 ipsumApp.events = () => {
-	$('#button__short').one('click', (e) => {
+	$("#button__short").one("click", (e) => {
 		e.preventDefault();
+		$(".singleButton").addClass("clicked");
 		newRandomArray = [];
 		shortParagraph();
 	});
-	$('#button__medium').one('click', (e) => {
+	$("#button__medium").one("click", (e) => {
 		e.preventDefault();
+		$(".singleButton").addClass("clicked");
 		newRandomArray = [];
 		mediumParagraph();
 	});
-	$('#button__long').one('click', (e) => {
+	$("#button__long").one("click", (e) => {
 		e.preventDefault();
+		$(".singleButton").addClass("clicked");
 		newRandomArray = [];
 		longParagraph();
+	});
+	$("#button__tryAgain").on("click", (e) => {
+		e.preventDefault();
+		newRandomArray = [];
+		window.location = "";
 	});
 }
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,7 +4,7 @@ var ipsumApp = {};
 
 var ipsumList = ["hoser", "two-four", "double-double", "homo milk", "keener", "kerfuffle", "Prime Minister", "beer", "hockey", "poutine", "moose", "igloo", "beaver", "loonie", "twoonie", "kilometres", "Canuck", "brown bread", "coffee whitener", "processed cheese", "brown bread", "rye & ginger", "chocolate bar", "public school", "supply teacher", "zed", "Girl Guides", "chesterfield", "housecoat", "hydro", "serviette", "washroom", "track pants", "Muskoka chair", "postal code", "beavertail", "ketchup chips", "butter tart", "nanaimo bar", "tortiere", "milk in a bag", "Smarties", "Crispy Crunch", "Coffee Crisp", "5-pin bowling", "Mountie", "toque", "plaid", "Wayne Gretzky", "aboot", "colour", "metric system", "health care", "zamboni", "Robertson screwdriver", "give'r", "snowbird", "head'r", "humidex", "processed cheese", "gitch", "runners", "garburator", "eavestrough", "knapsack", "beer store", "49th parallel", "runners", "parkade", "wicket", "pencil crayon", "bachelor apartment", "gasbar", "donair", "icing sugar", "fire hall", "hooped", "mickey", "twenty-sixer", "forty-pounder", "sixty-pounder", "Texas mickey", "pablum", "chip truck", "all-dressed chips", "take off", "freezies", "stagette", "turfed", "gotch", "maternity leave", "Timbit", "franglais", "Newfie", "Justin Trudeau", "maple syrup", "Kinder Surprise", "chinook", "deke", "Kraft Dinner", "lumberjack", "Log Driver's Waltz", "Degrassi", "sugar shack", "sorry", "Justin Bieber", "Celine Dion", "snowmobile", "cottage", "maple leaf", "O Canada", "Great Lakes", "canoe", "caesar", "Trans-Canada", "Prairies", "Maritimes", "Kodiak", "snow", "Bryan Adams", "Canadian Tire money", "CBC", "Peter Mansbridge", "Celine Dion", "province", "Ottawa", "Canada Day", "crokinole", "curling", "lacrosse", "Molson Canadian", "Kids in the Hall", "brews", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "New Brunswick", "Nova Scotia", "Newfoundland", "Prince Edward Island", "Anne of Green Gables", "Nunavut", "Northwest Territories", "Yukon Territory", "our home and native land", "we the north", "true north strong and free", "the Group of Seven", "brewski"];
 
-console.log('hello world');
+console.log("hello world");
 
 // create empty array for new words
 var newRandomArray = [];
@@ -39,7 +39,7 @@ var shortParagraph = function shortParagraph() {
 		generateRandomWords();
 	}
 	var finalShort = combinedSentences.join(" ");
-	$("section.displayIpsum").html("<div class='completeParagraph'><p> " + finalShort + " </p></div>");
+	$("section.displayIpsum").html("<div class=\"completeParagraph\"><p> " + finalShort + " </p></div>");
 };
 
 var mediumParagraph = function mediumParagraph() {
@@ -47,7 +47,7 @@ var mediumParagraph = function mediumParagraph() {
 		generateRandomWords();
 	}
 	var finalMedium = combinedSentences.join(" ");
-	$("section.displayIpsum").html("<div class='completeParagraph'><p> " + finalMedium + " </p></div>");
+	$("section.displayIpsum").html("<div class=\"completeParagraph\"><p> " + finalMedium + " </p></div>");
 };
 
 var longParagraph = function longParagraph() {
@@ -55,25 +55,33 @@ var longParagraph = function longParagraph() {
 		generateRandomWords();
 	}
 	var finalLong = combinedSentences.join(" ");
-	$("section.displayIpsum").html("<div class='completeParagraph'><p> " + finalLong + " </p></div>");
+	$("section.displayIpsum").html("<div class=\"completeParagraph\"><p> " + finalLong + " </p></div>");
 };
 
 // click events
 ipsumApp.events = function () {
-	$('#button__short').one('click', function (e) {
+	$("#button__short").one("click", function (e) {
 		e.preventDefault();
+		$(".singleButton").addClass("clicked");
 		newRandomArray = [];
 		shortParagraph();
 	});
-	$('#button__medium').one('click', function (e) {
+	$("#button__medium").one("click", function (e) {
 		e.preventDefault();
+		$(".singleButton").addClass("clicked");
 		newRandomArray = [];
 		mediumParagraph();
 	});
-	$('#button__long').one('click', function (e) {
+	$("#button__long").one("click", function (e) {
 		e.preventDefault();
+		$(".singleButton").addClass("clicked");
 		newRandomArray = [];
 		longParagraph();
+	});
+	$("#button__tryAgain").on("click", function (e) {
+		e.preventDefault();
+		newRandomArray = [];
+		window.location = "";
 	});
 };
 
